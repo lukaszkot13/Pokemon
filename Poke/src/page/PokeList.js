8dd8f8dbf8716325959ee625b820322aa3c2d2f7
lukaszkot13 <biuro.opolelubelskie@gmail.com>
2021-10-08 20:34:25 +0200

    zmiana

--- Poke/src/page/PokeList.js
+++ Poke/src/page/PokeList.js
@@ -1,29 +1,66 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function PokeList(){
-    const [pokemon, setPokemon]= useState(null)
-    const BASE_URL = (`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200
+import PokemonThumb from "../components/PokemonThub";
 
-    `)
+function PokeList() {
+  // const [chars, setChars] = useState(null)
+  const [pokemon, setPokemon] = useState();
+  const [limitValue, setLimitValue] = useState(15);
+  const [pageValue, setPageValue] = useState(0);
+  const BASE_URL = `https://pokeapi.co/api/v2/pokemon/`;
 
-    useEffect((pokemon)=>{
-        axios.get(`${BASE_URL}`).then((response) =>{
-            console.log('dane',response)
-            setPokemon(response.data.results)
-        })
-    },[])
-    if(!pokemon){
-        return(
-            <div>Brak banych</div>
-        )
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?limit=${limitValue}&offset=${pageValue}`)
+      .then((response) => {
+        console.log("dane", response.data);
+        setPokemon(response.data);
+        // setChars(response.data)
+      })
+      .catch((error) => console.log(error));
+  }, [pageValue]);
+
+  if (!pokemon) {
+    alert("Loading");
+  }
+
+  const prevPage = () => {
+    if (pageValue === 0) {
+      alert("To pierwsza strona!");
+      return;
     }
-//     axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=15`)
-//     .then(response => { setPokemon(response.data.results) 
-//         console.log(response.data.results)   
-// })
-// },[page])
 
+    setPageValue(pageValue - 16);
+    setLimitValue(15);
+  };
+  const nextPage = () => {
+    if (pageValue === 0) {
+      alert("zmiana!");
+      return;
+    }
 
+    setPageValue(pageValue + limitValue);
+    setLimitValue(15);
+  };
+  return (
+    <div>
+      {limitValue
+        .filter((item, index) => index < 15)
+        .map((pokemonStats, index) => (
+          <PokemonThumb
+            key={index}
+            id={pokemonStats.id}
+            image={pokemonStats.sprites.other.dream_world.front_default}
+            name={pokemonStats.name}
+            type={pokemonStats.types[0].type.name}
+            height={pokemonStats.height}
+            weight={pokemonStats.weight}
+            ability={pokemonStats.abilities[0].ability.name}
+            baseExperience={pokemonStats.base_experience}
+          />
+        ))}
+    </div>
+  );
 }
-export default PokeList
+export default PokeList;
